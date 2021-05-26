9238c567f89ca64bd7dbbff169eab9b19763ff51
alejandroOro <alejandro.orozco@revature.net>
2021-05-26 15:42:52 -0400

    signup

--- src/app/signup.service.ts
+++ src/app/signup.service.ts
@@ -7,12 +7,25 @@ import { User } from './user';
   providedIn: 'root',
 })
 export class SignupService {
+  private urlGetUserByEmail: string;
   private urlAddUser: string;
+  private urlGetUserByUsername: string;
+
   constructor(private http: HttpClient) {
     this.urlAddUser = 'http://localhost:9000/user/add';
+    this.urlGetUserByEmail = 'http://localhost:9000/user/email/';
+    this.urlGetUserByUsername = 'http://localhost:9000/user/username/';
   }
 
   public addUser(user: User): Observable<User> {
     return this.http.post<User>(this.urlAddUser, user);
+  }
+
+  public getUserByEmail(email: string): Observable<User> {
+    return this.http.get<User>(this.urlGetUserByEmail + email);
+  }
+
+  public getUserByUsername(username: string): Observable<User> {
+    return this.http.get<User>(this.urlGetUserByUsername + username);
   }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -15,12 +15,48 @@ export class SignupComponent implements OnInit {
   password2: string;
 
   user: User;
+  userEmailValidation: User;
+  userUsernameValidation: User;
 
   constructor(private signupService: SignupService) {
     this.user = new User();
   }
 
   ngOnInit(): void {}
+
+  validateCredentials(): void {
+    if (
+      this.userEmailValidation == null &&
+      this.userUsernameValidation == null
+    ) {
+      this.signupService.addUser(this.user).subscribe((res) => {
+        this.user = new User();
+      });
+      alert('user created');
+    } else {
+      if (this.userEmailValidation == null) {
+        alert('Username already in use');
+      } else if (this.userUsernameValidation == null) {
+        alert('Email already in use');
+      } else {
+        alert('Username and email already in use');
+      }
+    }
+  }
+
+  getUserByEmail(user: User): void {
+    this.signupService.getUserByEmail(user.email).subscribe((res) => {
+      this.userEmailValidation = res;
+      this.validateCredentials();
+    });
+  }
+
+  getUserByUserame(user: User): void {
+    this.signupService.getUserByUsername(user.username).subscribe((res) => {
+      this.userUsernameValidation = res;
+      this.getUserByEmail(user);
+    });
+  }
 
   submit(): void {
     this.errorState = false;
@@ -38,11 +74,8 @@ export class SignupComponent implements OnInit {
       this.errorMessage = 'Date of birth is required';
     } else {
       this.user.password = this.password1;
-      this.signupService.addUser(this.user).subscribe((res) => {
-        this.user = new User();
-      });
-
-      alert('user created');
+      // Method flow: getUserByUsername->getUserByEmail->validateCredentials
+      this.getUserByUserame(this.user);
     }
   }
 }
