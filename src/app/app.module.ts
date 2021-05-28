22e95ad3f57efe851ed2dfebe0e2523a3539d10d
Michael <michael.jim.harris@gmail.com>
2021-05-28 16:02:21 -0400

    login/error handling implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,10 @@ import { SignupComponent } from './signup/signup.component';
 import { FeedComponent } from './feed/feed.component';
 import { PostComponent } from './post/post.component';
 import { MainPageComponent } from './main-page/main-page.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { PosterComponent } from './poster/poster.component';
 import { FirstPageComponent } from './first-page/first-page.component';
+import { AppInterceptorService } from './app-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,13 @@ import { FirstPageComponent } from './first-page/first-page.component';
     CommonModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AppInterceptorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,15 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { LoggedUserService } from './logged-user.service';
+import { User } from './user';
+
+interface IUser {
+  username: string;
+  password: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
-  constructor(private loggedUser: LoggedUserService) {}
+  private urlLogin: string;
 
-  public login(username: string, password: string): boolean {
-    // temporary login logic
-    this.loggedUser.name = 'Morgan Freeman';
-    return username === 'password' && password === 'username';
+  constructor(private http: HttpClient) {
+    this.urlLogin = 'http://localhost:9000/user/login';
+  }
+
+  public login(user: IUser): Observable<User> {
+    return this.http.post<User>(this.urlLogin, user);
   }
 }
+
+//Mish's Code
+// export class LoginService {
+//   constructor(private loggedUser: LoggedUserService) {}
+
+//   public login(username: string, password: string): boolean {
+//     // temporary login logic
+//     this.loggedUser.name = 'Morgan Freeman';
+//     return username === 'password' && password === 'username';
+//   }
+// }
