8d3e3d83ece2d5c639c570f695f52f97a9d66daf
alejandroOro <alejandro.orozco@revature.net>
2021-05-28 17:56:42 -0400

    added more comment and followers

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { CommentService } from '../comment.service';
 import { Post } from '../post';
-
+import { Comment } from '../comment';
+import { LoggedUserService } from '../logged-user.service';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
@@ -8,8 +10,26 @@ import { Post } from '../post';
 })
 export class PostComponent implements OnInit {
   @Input() post: Post;
+  @Input() postId: Post;
+  userId: number;
+  showComments: boolean;
+  commentBody: string;
+  public currentPostId: number;
+  constructor(
+    private commentService: CommentService,
+    private loggedUser: LoggedUserService
+  ) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    this.userId = this.loggedUser.id;
+  }
 
-  ngOnInit(): void {}
+  public commentBtn() {
+    console.log(this.commentBody);
+    this.addComment(this.commentBody, this.userId, this.post.id);
+  }
+  private addComment(body: string, userId: number, postId: number) {
+    let newComment = new Comment(body, postId, userId);
+    this.commentService.addComment(newComment).subscribe((res) => {});
+  }
 }
