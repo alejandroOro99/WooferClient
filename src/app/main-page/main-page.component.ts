5907ba9a6ab9dde064c38dc5447a455b17b286f1
Mike <michael.d.keyes@gmail.com>
2021-05-31 10:23:41 -0400

    async pipe

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,6 +1,7 @@
 import { LocationStrategy } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Post } from '../post';
 import { PostService } from '../post.service';
 import { SignupService } from '../signup.service';
@@ -24,7 +25,7 @@ export class MainPageComponent implements OnInit {
   /**
    * list of posts that will be displayed
    */
-  posts: Post[] = [];
+  posts$: Observable<Post[]>;
   user: User;
   searchUser: boolean;
   isLoggedUser: boolean;
@@ -53,22 +54,8 @@ export class MainPageComponent implements OnInit {
     this.isLoggedUser =
       this.route.snapshot.paramMap.get('username') ===
       JSON.parse(localStorage.getItem('user')).username;
-    // if (localStorage.getItem('username') !== undefined) {
-    //   this.name = localStorage.getItem('name');
-    // }
-    // this.route.params.subscribe((p) => {
-    //   this.username = p.username;
-    // });
-    //   this.postService
-    //     .getByUsername(this.username)
-    //     .subscribe((res) => (this.feed = res));
-    //   this.preventBackButton();
-    //   this.searchUser = false;
-    this.postService
-      .getByUsername(this.route.snapshot.paramMap.get('username'))
-      .subscribe((res) => {
-        console.log('Fetched these posts: ' + res);
-        this.posts = res;
-      });
+    this.posts$ = this.postService.getByUsername(
+      this.route.snapshot.paramMap.get('username')
+    );
   }
 }
